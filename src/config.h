3dd4be96ff36305f86e4c76f41179d89df7a597f
Ramon Melo <me@ramonmelo.com>
2019-10-05 16:39:25 -0300

    CHANGED: Project configurations

--- src/config.h
+++ src/config.h
@@ -1,9 +1,9 @@
 // MONAR Config File
 
-#define WIFI_NAME "HAL"
+#define WIFI_SSID "HAL"
 #define WIFI_PASS "CanionLabs0987"
 
-#define MQTT_BROKER "platform.canionlabs.io"
+// #define MQTT_BROKER "platform.canionlabs.io"
 #define MQTT_USER "c5ee80c0-e79b-11e9-b4f0-351ee008c4a9"
 #define MQTT_PASS "0hks3CUTMslYGUNf4dGV"
 #define MQTT_PORT 1883
@@ -13,5 +13,5 @@
 // #define MES_CFG_CONFIRM_TOPIC "canionlabs/mes/cfg/confirm"
 // #define MES_DATA_TOPIC "canionlabs/mes/data"
 
-#define MONAR_DEVICE_ID "device1234"
-#define MONAR_WILL_TOPIC "canionlabs/mes/will"
+// #define MONAR_DEVICE_ID "device1234"
+#define MONAR_DATA_TOPIC "v1/devices/me/telemetry"
