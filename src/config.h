9cafddac28dc88f426c867954771ebc394fdb336
Ramon Melo <me@ramonmelo.com>
2019-10-05 15:18:49 -0300

    CHANGED: Setup configuration variables

--- src/config.h
+++ src/config.h
@@ -3,5 +3,15 @@
 #define WIFI_NAME "HAL"
 #define WIFI_PASS "CanionLabs0987"
 
-#define TB_URL "platform.canionlabs.io"
-#define TB_USER ""
+#define MQTT_BROKER "platform.canionlabs.io"
+#define MQTT_USER "c5ee80c0-e79b-11e9-b4f0-351ee008c4a9"
+#define MQTT_PASS "0hks3CUTMslYGUNf4dGV"
+#define MQTT_PORT 1883
+
+// #define MES_STATE_TOPIC "canionlabs/mes/status"
+// #define MES_CFG_TOPIC "canionlabs/mes/cfg"
+// #define MES_CFG_CONFIRM_TOPIC "canionlabs/mes/cfg/confirm"
+// #define MES_DATA_TOPIC "canionlabs/mes/data"
+
+#define MONAR_DEVICE_ID "device1234"
+#define MONAR_WILL_TOPIC "canionlabs/mes/will"
