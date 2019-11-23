f471616f2e91e0b95e2044af75278b9a34129f14
Ramon Melo <me@ramonmelo.com>
2019-11-23 09:21:39 -0300

    CHANGED: Update public variables

--- src/config.h
+++ src/config.h
@@ -1,11 +1,8 @@
 // MONAR Config File
 
-#define WIFI_SSID "canionlabs"
-#define WIFI_PASS "canionlabs1234"
-
 #define MQTT_BROKER ""
-#define MQTT_ID "fcb637c0-ff69-11e9-8ad7-1d31d20907a4" 
-#define MQTT_USER "nXLetXHwv47c6JxveaVc"
+#define MQTT_ID "" 
+#define MQTT_USER ""
 #define MQTT_PORT 1883
 
 #define DATA_TOPIC "v1/devices/me/telemetry"
